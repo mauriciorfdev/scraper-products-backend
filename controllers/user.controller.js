7f5d46ab44ca9520c4f374af444e0cdf0252eade
m <139261114+mauriciorfdev@users.noreply.github.com>
2026-05-31 18:16:00 -0400

    feat: add getProfile controller and authMiddleware route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,18 @@
 import { UserModel } from '../models/user.model.js';
 
-// DESC Get All Users
-// ROUTE GET /api/users
+// GET /api/users
 async function getUsers(req, res) {
   const users = await UserModel.find().select('name email');
 
   return res.status(200).json(users);
 }
 
-export { getUsers };
+// GET /api/users/profile
+async function getProfile(req, res) {
+  const user = await UserModel.findById(req.user.userId).select('-password');
+  if (!user) return res.status(401).json({ msg: 'Unauthorized' });
+  req.user = user;
+  res.status(200).json({ msg: req.user });
+}
+
+export { getUsers, getProfile };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getUsers } from '../controllers/user.controller.js';
+import { getUsers, getProfile } from '../controllers/user.controller.js';
+import { authMiddleware } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
+
+router.get('/profile', authMiddleware, getProfile);
 
 export default router;
