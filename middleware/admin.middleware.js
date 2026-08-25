12688afbc1df59c758dab1fc85851e25fefbbd84
m <139261114+mauriciorfdev@users.noreply.github.com>
2026-08-24 23:16:19 -0400

    chore(config): add production cors support
    - add env variable FRONTEND_URL for CORS
    - add start script to package.json
    - rename response keys in middleware 'msg' to 'message'

--- middleware/admin.middleware.js
+++ middleware/admin.middleware.js
@@ -3,7 +3,7 @@ import { UserModel } from '../models/user.model.js';
 async function adminMiddleware(req, res, next) {
   const user = await UserModel.findById(req.user.userId).select('role');
   if (user.role !== 'admin') {
-    return res.status(403).json({ msg: 'Admin access required' });
+    return res.status(403).json({ message: 'Admin access required' });
   }
   next();
 }
